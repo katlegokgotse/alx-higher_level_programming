8bf9916868b6a8abfce26e8a48d3b4a55806572d
Katlego Kgotse <katlegokgotse88@gmail.com>
2025-03-24 07:53:36 -0700

    1 Last Digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ lastNumber = number % 10
 position = ""
 if lastNumber > 5:
     position = "and is greater than 5"
-elif lastNumber<6 and lastNumbe!=0:
+elif lastNumber<6 and lastNumber!=0:
     position = "and is less than 6 and not 0"
 else:
     position="and is 0"
