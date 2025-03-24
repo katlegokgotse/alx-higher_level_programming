fc46f855d4d05513df922e2d3c5e23e6f2a9789c
Katlego Kgotse <katlegokgotse88@gmail.com>
2025-03-24 07:59:58 -0700

    changes

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,6 +4,8 @@ number = random.randint(-10000, 10000)
 # YOUR CODE HERE
 lastNumber = int(repr(number)[-1])
 position = ""
+if number < 0:
+    lastNumber = -lastNumber
 if lastNumber > 5:
     position = "and is greater than 5"
 elif lastNumber < 6 and lastNumber != 0:
