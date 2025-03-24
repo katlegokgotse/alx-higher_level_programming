975b8bed3a56621e256602e93730e69636447595
Katlego Kgotse <katlegokgotse88@gmail.com>
2025-03-24 07:54:38 -0700

    1 Last Digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,8 +6,8 @@ lastNumber = number % 10
 position = ""
 if lastNumber > 5:
     position = "and is greater than 5"
-elif lastNumber<6 and lastNumber!=0:
+elif lastNumber < 6 and lastNumber!= 0:
     position = "and is less than 6 and not 0"
 else:
-    position="and is 0"
+    position = "and is 0"
 print(f"Last digit of {number} is {lastNumber} {position}")
